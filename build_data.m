5f70dde5b719776aebd76daa97a4fc57455f532f
TravisHester-L <twhester@mail.lipscomb.edu>
2022-04-23 18:56:09 -0500

    Added build file for fft data

---
+++ build_data.m
@@ -0,0 +1,53 @@
+fs = 48000;     % sampling frequency
+time = 5;       % recording time in seconds
+L = time*fs;    % # of samples in a recording
+
+% get list of sample files
+sampFiles = dir("samples");
+sampFiles = sampFiles(3:end);
+
+% initialize cell arays
+sample_data = {};
+fft_data = {};
+
+% iterate over all files
+for i = 1:length(sampFiles)
+    % get the name of each file
+    fieldName = split(sampFiles(i).name, ".");
+    fieldName = fieldName(1);
+    
+    % extract the subject's name
+    subjectName = split(fieldName, "_");
+    subjectName = subjectName(3);
+    
+    % get the list of samples
+    samples = load("samples/" + sampFiles(i).name);
+    samples = getfield(samples, fieldName{1});
+    
+    % store sample data with subject name in cell array
+    sample_data{i} = {subjectName{1}, samples};
+    
+    % array to hold fft data for each sample
+    transformedData = zeros(size(samples, 1), size(samples, 2)/2 + 1);
+    
+    % iterate over all samples
+    for j = 1:size(samples, 1)
+        % perform fft
+        Y = fft(samples(j, :));
+        
+        % normalize and scale
+        P2 = abs(Y/L);          
+        P1 = P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+        
+        % add fft data to array
+        transformedData(j, :) = P1;
+    end
+    
+    % store fft data with subject name in cell array
+    fft_data{i} = {subjectName{1}, transformedData};
+end
+
+% save variables to .mat files
+save('sample_data.mat', 'sample_data');
+save('fft_data.mat', 'fft_data');
