8749715212422c890b5de955ce16fcebb2ed0e11
willwelch2000 <willwelch2000@gmail.com>
2022-04-23 16:36:45 -0500

    New comparison functions

---
+++ compare_shifted_diff.m
@@ -0,0 +1,21 @@
+function score = compare_shifted_diff(comparison, new_fft)
+    % Scale both to have one as max
+    comparison = comparison/max(comparison);
+    new_fft = new_fft/max(new_fft);
+
+    % Find (first) max location of each
+    peak_comparison = find(comparison == 1);
+    peak_comparison = peak_comparison(1);
+    peak_new_fft = find(new_fft == 1);
+    peak_new_fft = peak_new_fft(1);
+
+    % Shift the one that's farther down so the peaks match
+    if (peak_comparison > peak_new_fft)
+        comparison = comparison(peak_comparison-peak_new_fft:end);
+    else
+        new_fft = new_fft(peak_new_fft-peak_comparison+1:end);
+    end
+
+    % Use simple difference compare function
+    score = compare_simple_diff(comparison, new_fft);
+end
---
+++ compare_simple_diff.m
@@ -0,0 +1,16 @@
+function score = compare_simple_diff(comparison, new_fft)
+    % Only consider up to 4000Hz
+    max_freq = 4000;
+    comparison = comparison(1:max_freq);
+    new_fft = new_fft(1:max_freq);
+
+    % Scale both to have one as max
+    comparison = comparison/max(comparison);
+    new_fft = new_fft/max(new_fft);
+
+    % Find absolute value of difference for each point
+    difference = abs(comparison-new_fft);
+
+    % Find total difference
+    score = sum(difference);
+end
